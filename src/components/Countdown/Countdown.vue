b8d1315be22ceb97b4e743a689955de93a1a0142
jimoh-sodiq <jimohsodiq301@gmail.com>
2025-01-02 14:13:36 +0100

    re-add countdown component

---
+++ src/components/Countdown/Countdown.vue
@@ -0,0 +1,97 @@
+<template>
+  <div style="justify-content: space-around;">
+    <div v-for="(unit, label) in visibleComponents" :key="label">
+      {{ unit }} {{ label }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref, computed, onMounted, watch, onBeforeUnmount} from "vue";
+
+
+const emit = defineEmits(['remaining-seconds', 'countdown-ended'])
+
+interface Props {
+  endDate: Date;
+  negative?: boolean;
+  accuracy?: 1 | 2 | 3 | 4;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  negative: false,
+  accuracy: 4
+})
+
+const timer = ref()
+const now = ref(new Date())
+
+const timeComponents = computed(() => {
+  const totalSeconds = Math.max(
+      (props.endDate.getTime() - now.value.getTime()) / 1000,
+      props.negative ? (props.endDate.getTime() - now.value.getTime()) / 1000 : 0
+  );
+
+  const days = Math.trunc(totalSeconds / 86400);
+  const hours = Math.trunc((totalSeconds % 86400) / 3600);
+  const minutes = Math.trunc((totalSeconds % 3600) / 60);
+  const seconds = Math.trunc(totalSeconds % 60);
+
+  return {
+    days, hours, minutes, seconds
+  }
+})
+
+const visibleComponents = computed(() => {
+
+  const allComponents = [
+    { label: timeComponents.value.days > 1 ? "days" : "day", val: timeComponents.value.days },
+    { label: timeComponents.value.hours > 1 ? "hours": "hour", val: timeComponents.value.hours },
+    { label: timeComponents.value.minutes > 1 ? "minutes": "minute", val: timeComponents.value.minutes },
+    { label: timeComponents.value.seconds > 1 ? "seconds" : "second", val: timeComponents.value.seconds },
+  ];
+
+  // Filter out components with a value of 0 unless accuracy allows
+  const nonZeroComponents = allComponents.filter((comp) => comp.val > 0);
+
+  // Limit the display to the number of components specified by `accuracy`
+  return Object.fromEntries(
+      nonZeroComponents.slice(0, props.accuracy).map((comp) => [comp.label, formatTimeUnit(comp.val)])
+  );
+})
+
+function formatTimeUnit(timeUnit: number) {
+  return timeUnit > 9 ? timeUnit : '0' + timeUnit;
+}
+
+
+onMounted(() => {
+  if(!props.negative && new Date().getTime() > props.endDate.getTime()){
+    emit('countdown-ended')
+  }
+})
+
+
+watch(() => props.endDate, (newValue, oldValue) => {
+  if (timer.value){
+    clearInterval(timer.value)
+  }
+  timer.value = setInterval(() => {
+    now.value = new Date();
+    if(!props.negative && now.value > newValue){
+      now.value = newValue;
+      emit('countdown-ended')
+      clearInterval(timer.value)
+    }
+  }, 1000)
+}, {immediate: true})
+
+watch(timeComponents, () => {
+  emit('remaining-seconds', Math.trunc((props.endDate.getTime() - now.value.getTime()) / 1000) )
+}, {immediate: true})
+
+onBeforeUnmount(() => {
+  clearInterval(timer.value);
+})
+
+</script>
---
+++ src/demos/Countdown.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Countdown from '../components/Countdown/Countdown.vue';
+
+const endDate = ref(new Date(Date.now() + 3600000))
+const accuracy = ref(4)
+</script>
+
+<template>
+    <div class="p-3 mt-4">
+
+        <p>Countdown ends in: </p>
+
+        <div class="bg-gray-300 p-4 rounded font-medium text-gray-700">
+            <Countdown :endDate="new Date(endDate)" :accuracy="accuracy" />
+        </div>
+
+        <p class="my-4">Select End Date: {{ new Date(endDate) }}</p>
+        <input type="date" v-model="endDate" class="rounded  w-full p-4 ring-[2px] ring-gray-400" />
+
+        <p>Select Accuracy</p>
+        <select v-model.number="accuracy" class="appearance-auto p-4 w-full rounded">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+        </select>
+        <p>Accuracy controls if day, hour, minute or second should be shown, 1 = show the max time available only, and so on</p>
+        <p>Example: An accuracy of 3 will will show days, hours minute, if days is not possible, it shows hour, minutes, second </p>
+
+    </div>
+</template>
